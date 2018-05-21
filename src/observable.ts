2e3ccf9b4643c546578bab7702e08bd622e8d62f
jordanhennell <jordan.hennell@hotmail.com>
2018-05-21 15:07:56 +1000

    Symbol.observable implementation

--- src/observable.ts
+++ src/observable.ts
@@ -1,19 +1,22 @@
-export type ObservableConstructor<T> = new (subscribe: (observer: IObserver<T>) => Unsubscribe) => IObservable<T>;
+import "symbol-observable";
 
 // Based on https://github.com/tc39/proposal-observable
-export interface IBaseObservable<T> {
-    subscribe(next: (value: T) => void, error?: (err: any) => void, complete?: () => void): ISubscription;
+export interface IObservable<T> {
+    subscribe(observer?: IObserver<T>): ISubscription;
+    subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): ISubscription;
+    subscribe(nextOrObserver?: IObserver<T> | ((value: T) => void), error?: (error: any) => void, complete?: () => void): ISubscription;
 }
 
-export interface IObservable<T> extends IBaseObservable<T> {
-    filter(condition: (value: T) => boolean): IObservable<T>;
-    map<U>(select: (value: T) => U): IObservable<U>;
-    resolved<P>(this: IObservable<Promise<P>>): IObservable<P>;
-    rejected(this: IObservable<Promise<any>>): IObservable<any>;
-    asObservable(): IObservable<T>;
-    merge<O>(this: IObservable<IBaseObservable<O>>): IObservable<O>;
-    errored(this: IObservable<IBaseObservable<any>>): IObservable<any>;
-    completed(this: IObservable<IBaseObservable<any>>): IObservable<void>
+export interface ISimpleObservable<T> extends IObservable<T> {
+    filter(condition: (value: T) => boolean): ISimpleObservable<T>;
+    map<U>(select: (value: T) => U): ISimpleObservable<U>;
+    resolved<P>(this: ISimpleObservable<Promise<P>>): ISimpleObservable<P>;
+    rejected(this: ISimpleObservable<Promise<any>>): ISimpleObservable<any>;
+    asObservable(): ISimpleObservable<T>;
+    merge<O>(this: ISimpleObservable<IObservable<O>>): ISimpleObservable<O>;
+    errored(this: ISimpleObservable<IObservable<any>>): ISimpleObservable<any>;
+    completed(this: ISimpleObservable<IObservable<any>>): ISimpleObservable<void>;
+    [Symbol.observable](): IObservable<T>; // uses symbol-observable ponyfill
 }
 
 export interface ISubscription {
@@ -24,7 +27,7 @@ export interface ISubscription {
 export type Unsubscribe = () => void;
 
 export interface IObserver<T> {
-    next(value: T): void;
+    next?(value: T): void;
     error?(err: any): void;
     complete?(): void;
     closed?: boolean;
@@ -32,84 +35,80 @@ export interface IObserver<T> {
 
 export type SubscriberFunction<T> = (observer: IObserver<T>) => ISubscription;
 
-class SimpleObservable<T> implements IObservable<T> {
+class SimpleObservable<T> implements ISimpleObservable<T> {
     constructor(private _subscribe: SubscriberFunction<T>) { }
 
+    [Symbol.observable](): IObservable<T> { return this };
 
-    subscribe(next: (value: T, error?: (err: any) => void, complete?: () => void) => void) {
-        return this._subscribe({ next });
+    subscribe(observer?: IObserver<T>): ISubscription;
+    subscribe(next?: (value: T) => void, error?: (error: any) => void, complete?: () => void): ISubscription;
+    subscribe(nextOrObserver?: IObserver<T> | ((value: T) => void), error?: (error: any) => void, complete?: () => void): ISubscription {
+        return this.isObserver(nextOrObserver)
+            ? this._subscribe({ ...nextOrObserver })
+            : this._subscribe({ next: nextOrObserver, error, complete});
+    }
+
+    private isObserver<T>(value?: any): value is IObserver<T> {
+        return !!value 
+            && ("next" in value 
+            || "error" in value 
+            || "complete" in value 
+            || "closed" in value);
     }
 
     filter(condition: (value: T) => boolean) {
-        return new Observable<T>(observer => this.subscribe(value => condition(value) && observer.next(value)));
+        return new Observable<T>(observer => this.subscribe(value => condition(value) && observer.next && observer.next(value)));
     }
 
     map<U>(select: (value: T) => U) {
-        return new Observable<U>(observer => this.subscribe(value => observer.next(select(value))));
+        return new Observable<U>(observer => this.subscribe(value => observer.next && observer.next(select(value))));
     }
 
-    resolved<P>(this: IObservable<Promise<P>>) {
-        return new Observable<P>(observer => this.subscribe(value => value.then(result => observer.next(result))));
+    resolved<P>(this: ISimpleObservable<Promise<P>>) {
+        return new Observable<P>(observer => this.subscribe(value => value.then(result => observer.next && observer.next(result))));
     }
 
-    rejected(this: IObservable<Promise<any>>) {
-        return new Observable<any>(observer => this.subscribe(value => value.catch(error => observer.next(error))));
+    rejected(this: ISimpleObservable<Promise<any>>) {
+        return new Observable<any>(observer => this.subscribe(value => value.catch(error => observer.next && observer.next(error))));
     }
 
     asObservable() {
-        return new Observable<T>(observer => this.subscribe(v => observer.next(v)));
+        return new Observable<T>(observer => this.subscribe(v => observer.next && observer.next(v)));
     }
 
-    merge<O>(this: IObservable<IBaseObservable<O>>) {
-        return new Observable<O>(observer => {
-            let subscriptions = [] as ISubscription[];
-            this.subscribe(value => subscriptions.push(value.subscribe(
-                v => observer.next(v)
-            )));
-            return {
-                unsubscribe: () => subscriptions.forEach(u => u.unsubscribe()),
-                closed: subscriptions.every(u => u.closed)
-            }
-        });
+    merge<O>(this: ISimpleObservable<IObservable<O>>) {
+        return new Observable<O>(observer =>
+            this.subscribe(value => value.subscribe(observer))
+        );
     }
 
-    errored(this: IObservable<IBaseObservable<any>>) {
-        return new Observable<any>(observer => {
-            let subscriptions = [] as ISubscription[];
-            this.subscribe(value => subscriptions.push(value.subscribe(
-                () => {},
-                e => observer.next(e)
-            )));
-            return {
-                unsubscribe: () => subscriptions.forEach(u => u.unsubscribe()),
-                closed: subscriptions.every(u => u.closed)
-            }
-        });
+    errored(this: ISimpleObservable<IObservable<any>>) {
+        return new Observable<any>(observer =>
+            this.subscribe(value => value.subscribe(
+                () => { },
+                observer.next
+            ))
+        );
     }
 
-    completed(this: IObservable<IBaseObservable<any>>) {
-        return new Observable<void>(observer => {
-            let subscriptions = [] as ISubscription[];
-            this.subscribe(value => subscriptions.push(value.subscribe(
-                () => {},
-                () => {},
-                () => observer.next(void 0)
-            )));
-            return {
-                unsubscribe: () => subscriptions.forEach(u => u.unsubscribe()),
-                closed: subscriptions.every(u => u.closed)
-            }
-        });
+    completed(this: ISimpleObservable<IObservable<any>>) {
+        return new Observable<void>(observer =>
+            this.subscribe(value => value.subscribe(
+                () => { },
+                () => { },
+                () => observer.next && observer.next(void 0)
+            ))
+        );
     }
 }
 
-export let Observable: { new <T>(subscribe: (observer: IObserver<T>) => ISubscription): IObservable<T> } = SimpleObservable;
+export let Observable: { new <T>(subscribe: (observer: IObserver<T>) => ISubscription): ISimpleObservable<T> } = SimpleObservable;
 
 export function useObservableType(observableImplementation: typeof Observable) {
     Observable = observableImplementation;
     Object.getOwnPropertyNames(SimpleObservable.prototype).forEach(prop => {
         if (!(prop in Observable.prototype))
-            Observable.prototype[prop] = SimpleObservable.prototype[prop as keyof IObservable<any>];
+            Observable.prototype[prop] = SimpleObservable.prototype[prop as keyof ISimpleObservable<any>];
     });
 }
 
