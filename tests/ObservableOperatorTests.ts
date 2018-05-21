680d70e1a88539398e345afc3dc422bb55da92c7
jordanhennell <jordan.hennell@hotmail.com>
2018-05-21 17:05:02 +1000

    Tests for observable errored and completed

--- tests/ObservableOperatorTests.ts
+++ tests/ObservableOperatorTests.ts
@@ -2,6 +2,7 @@ import { SynchronousPromise } from "synchronous-promise";
 import { it, test } from "wattle";
 import { IObservable, Observable } from "../src/observable";
 import './setup';
+import sinon = require("sinon");
 
 export function testObservableOperators() {
     test("filter", () => {
@@ -50,26 +51,58 @@ export function testObservableOperators() {
         });
     });
 
-    test("subscribeInner", () => {
+    test("merge", () => {
         let source = observableOf(
             observableOf(1),
             observableOf(2),
             observableOf(3),
             observableOf(4)
         );
-        let result = source.subscribeInner();
+        let result = source.merge();
 
         values(result).should.have.members([1, 2, 3, 4]);
     });
 
+    test("errored", () => {
+        let errorMessage = "error message";
+        let source = observableOf(
+            new Observable<number>(observer => {
+                observer.error && observer.error(errorMessage)
+                return emptyUnsubscribe();
+            }),
+            observableOf(2));
+        let result = source.errored();
+
+        values(result).should.have.members([errorMessage]);
+    });
+
+    test("completed", () => {
+        let unresolvedPromise = SynchronousPromise.unresolved<number>();
+        let completedSpy = sinon.spy();
+        let source = observableOf(new Observable<number>(observer => {
+            unresolvedPromise.then(v => observer.complete && observer.complete());
+            return emptyUnsubscribe();
+        }));
+
+        let result = source.completed();
+        result.subscribe(o => completedSpy());
+        completedSpy.should.not.have.been.called;
+        unresolvedPromise.resolve();
+        completedSpy.should.have.been.called;
+    });
+
     function observableOf<T>(...args: T[]) {
         return new Observable<T>(observer => {
-            args.forEach(a => observer.next(a));
-            return {
-                unsubscribe: () => { },
-                closed: false
-            };
+            args.forEach(a => observer.next && observer.next(a));
+            return emptyUnsubscribe();
         });
+    }
+
+    function emptyUnsubscribe() {
+        return {
+            unsubscribe: () => { },
+            closed: false
+        }
     }
 
     function values<T>(observable: IObservable<T>) {
