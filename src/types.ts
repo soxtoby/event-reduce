ae59d93fe000d6a9a847cf090c31a0fd59068af3
Jordan <jordan.hennell@hotmail.com>
2018-06-07 11:47:48 +1000

    Typescript 2.9 keyof changes (#6)
    * Typescript 2.9 keyof changes

--- src/types.ts
+++ src/types.ts
@@ -5,7 +5,7 @@
  *  - T & never = never
  *  - An object with a string index signature can be indexed with any string.
  */
-export declare type StringDiff<T extends string, U extends string> = ({
+export declare type KeyDiff<T extends string | number | symbol, U extends string | number | symbol> = ({
     [K in T]: K;
 } & {
     [K in U]: never;
@@ -16,10 +16,10 @@ export declare type StringDiff<T extends string, U extends string> = ({
  * From https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
  * Omits keys in K from object type T
  */
-export declare type ObjectOmit<T extends object, K extends keyof T> = Pick<T, StringDiff<keyof T, K>>;
+export declare type ObjectOmit<T extends object, K extends keyof T> = Pick<T, KeyDiff<keyof T, K>>;
 /**
  * Returns a version of type T where all properties which are also in U are optionalized.
- * Useful for makding props with defaults optional in React components.
+ * Useful for making props with defaults optional in React components.
  * Compare to flow's $Diff<> type: https://flow.org/en/docs/types/utilities/#toc-diff
  */
 export declare type ObjectDiff<T extends object, U extends object> = ObjectOmit<T, keyof U & keyof T> & {
