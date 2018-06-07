b7d0f2f74c7c690f7422594fc4cc982990c6ced4
jordanhennell <jordan.hennell@hotmail.com>
2018-06-07 11:31:38 +1000

    Typescript 2.9 keyof changes

--- src/types.ts
+++ src/types.ts
@@ -16,12 +16,14 @@ export declare type StringDiff<T extends string, U extends string> = ({
  * From https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-311923766
  * Omits keys in K from object type T
  */
-export declare type ObjectOmit<T extends object, K extends keyof T> = Pick<T, StringDiff<keyof T, K>>;
+export declare type ObjectOmit<T extends object, K extends StringKeyOf<T>> = Pick<T, StringDiff<StringKeyOf<T>, K>>;
 /**
  * Returns a version of type T where all properties which are also in U are optionalized.
  * Useful for makding props with defaults optional in React components.
  * Compare to flow's $Diff<> type: https://flow.org/en/docs/types/utilities/#toc-diff
  */
-export declare type ObjectDiff<T extends object, U extends object> = ObjectOmit<T, keyof U & keyof T> & {
+export declare type ObjectDiff<T extends object, U extends object> = ObjectOmit<T, StringKeyOf<U> & StringKeyOf<T>> & {
     [K in (keyof U & keyof T)]?: T[K];
 };
+
+export type StringKeyOf<T> = Extract<keyof T, string>; 
