683c7430387a16acc918254ec5fb710b19926e15
Simon Oxtoby <soxtoby@zaptechnology.com>
2021-03-03 16:18:05 +1000

    Redux devtools integration doesn't copy properties from event, so if it's a model, the correct properties will be serialized

--- packages/event-reduce/src/devtools.ts
+++ packages/event-reduce/src/devtools.ts
@@ -26,12 +26,12 @@ export function sendEvent(name: string, arg: any) {
     if (process.env.NODE_ENV !== 'production') {
         if (modelDevTools.size) {
             let event = isObject(arg)
-                ? { ...arg }
+                ? getState(arg)
                 : { value: arg };
             if ('type' in event)
                 event['.type'] = event.type;
             event.type = name;
-            modelDevTools.forEach((dev, model) => dev.send(getState(event), getState(model, true)));
+            modelDevTools.forEach((dev, model) => dev.send(event, getState(model, true)));
         }
     }
 }
