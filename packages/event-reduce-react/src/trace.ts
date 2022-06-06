c8c119f9b8e0f0ceb2c4353e8ad5c27334aea1a5
jordanhennell <jordan.hennell@hotmail.com>
2022-06-06 20:04:48 +1000

    Remove integration with react-dev-tools tracing, it no longer exists
    Was removed in React 17 https://github.com/facebook/react/pull/20037

--- packages/event-reduce-react/src/trace.ts
+++
@@ -1,4 +0,0 @@
-import { unstable_trace } from "scheduler/tracing";
-
-export const trace = unstable_trace
-    || function trace<T>(name: string, timestamp: number, callback: () => T, threadID?: number) { return callback(); }
