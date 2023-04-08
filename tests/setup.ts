e407531c384b39a678c345ba275489369f80a2e5
Simon Oxtoby <simonoxtoby@gmail.com>
2023-04-08 11:53:20 +1000

    Using synchronous reaction queue in tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,6 +2,7 @@ import "jsdom-global/register";
 import { cleanup } from "@testing-library/react";
 import { should, use } from 'chai';
 import * as chaiDom from "chai-dom";
+import { PromiseQueue, reactionQueue } from "event-reduce/lib/reactions";
 import * as sinonChai from 'sinon-chai';
 import { SynchronousPromise } from "synchronous-promise";
 import { ITest, ITestContext, TestMiddleware } from "wattle";
@@ -9,6 +10,8 @@ import { ITest, ITestContext, TestMiddleware } from "wattle";
 should();
 use(sinonChai);
 use(chaiDom);
+
+reactionQueue.current = new PromiseQueue(SynchronousPromise.resolve());
 
 class Middleware extends TestMiddleware {
     override run(test: ITest, context: ITestContext, next: () => void) {
